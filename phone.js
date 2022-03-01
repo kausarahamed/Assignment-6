3c30874682e5430420a9fc033c71e164b6fc8e09
kausarahamed <galibhasan517@gmail.com>
2022-03-02 01:24:46 +0600

    last night

--- phone.js
+++ phone.js
@@ -1,6 +1,11 @@
+const searchField = document.getElementById('search-field');
+const searchResult = document.getElementById('search-result');
+const details =document.getElementById('details-Up');
+
 const searchPhone = () => {
 
-    const searchField = document.getElementById('search-field');
+    
+
     const searchText = searchField.value;
 
     searchField.value = '';
@@ -16,20 +21,20 @@ const searchPhone = () => {
 
 const displaySearchResult = phones=>{
 
-    // console.log(phones);
+   
 
-    const searchResult = document.getElementById('search-result');
+    
     phones.forEach(phone => {
-    //    console.log(phone);
+    
        
        const div = document.createElement('div');
-        // div.classList.add('col');
+        
         div.innerHTML=`
             <div class="bg-red-200 p-10">
                 <img src="${phone.image}" class="mx-auto w-5/6 mb-4 rounded-2xl" alt="...">
-                    <div class="card-body">
-                        <h4 class="">${phone.phone_name}</h4>
-                        <h5 class="mb-5">${phone.brand}</h5>
+                    <div>
+                        <h3 class="text-2xl font-semibold">${phone.phone_name}</h3>
+                        <h4 class="mb-5 text-xl font-semibold">${phone.brand}</h4>
                         
                         <a onclick="loadPhoneDetail('${phone.slug}')" href="#" class="border-2 p-2 text-xl  rounded-xl">Details</a>
                     </div>
@@ -51,33 +56,32 @@ const loadPhoneDetail = phoneId =>{
 
 }
 
-const details =document.getElementById('details-Up');
+
 
 const displayDetails = (phone) =>{
     console.log(phone);
-const {name,releaseDate,image,brand,mainFeatures,others} = phone;
+const {name,image,brand,mainFeatures,others} = phone;
 
 
 
     const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
-            <div class="card">
-                <img src="${image}" class="w-3/4" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">${name}</h4>
-                        <h5 class="card-title">${releaseDate}</h5>
-
-                        <h5 class="text-xl">${brand} </h5>
-                        <p class="text-xl">${mainFeatures.displaySize}</p>
-                        <p class="text-xl">${mainFeatures.storage}</p>
-                        <p class="text-xl">${mainFeatures.chipSet}</p>
-                        <p class="text-xl">${mainFeatures.memory}</p>
-                        <p class="text-xl">${mainFeatures.sensors}</p>
-                        <p class="text-xl">${others.Bluetooth}</p>
-                        <p class="text-xl">${others.GPS}</p>
-                        <p class="text-xl">${others.USB}</p>
-                        <p class="text-xl">${others.Radio}</p>
+            <div class='border-2 text-center mb-5 bg-gray-300 rounded-2xl'>
+                <img src="${image}" class="w-3/4 mx-auto rounded-2xl text-center" alt="...">
+                    <div class="text-left">
+                        <p class="text-3xl font-bold">Model: ${name}</p>
+                        <p class="text-2xl font-semibold">Brand: ${brand} </p>
+                         <p class="text-xl"><span class="text-left font-semibold">Release Date:</span> ${phone?.releaseDate || 'Release date not found'}</p>
+                        <p class="text-xl"><span class="font-semibold">Display size:</span> ${mainFeatures.displaySize}</p>
+                        <p class="text-xl"><span class="font-semibold">Storege:</span> ${mainFeatures.storage}</p>
+                        <p class="text-xl"><span class="font-semibold">ChipSet:</span> ${mainFeatures.chipSet}</p>
+                        <p class="text-xl"><span class="font-semibold">Memory:</span> ${mainFeatures.memory}</p>
+                        <p class="text-xl"><span class="font-semibold">Sensors:</span> ${mainFeatures.sensors}</p>
+                        <p class="text-xl"><span class="font-semibold">Bluetooth:</span> ${others.Bluetooth}</p>
+                        <p class="text-xl"><span class="font-semibold">GPS:</span> ${others.GPS}</p>
+                        <p class="text-xl"><span class="font-semibold">USB:</span> ${others.USB}</p>
+                        <p class="text-xl"><span class="font-semibold">Radio:</span> ${others.Radio}</p>
                         
                     </div>
               </div>
